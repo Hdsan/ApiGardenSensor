1332cd9af14723dbf0d61b3d16e9c914d88bcf98
Hudson Ferreira <hudsonferreira2501@gmail.com>
2026-04-19 20:28:52 -0300

    rota de amostra

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 import readServices from "../services/readServices.js";
 
@@ -10,12 +10,16 @@ router.post("/air", async (req, res) => {
   const result = await readServices.storeSensorInfos(req.body);
   res.status(200).json(result);
 });
+router.post("/sample", async (req, res) => {
+  console.log("ESP32 online....")
+  console.log(req.body);
+  res.status(200).json(true);
+});
 
 router.get("/", async (req, res) => {
   const { bedId } = req.query;
   const response = await readServices.getReadInfos(bedId);
   res.json(response);
 });
-
 
 export default router;
