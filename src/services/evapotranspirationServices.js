d0451dfe7251cca6ec40142a12de1f67390c5372
Hudson Ferreira <hudsonferreira2501@gmail.com>
2026-05-04 16:01:59 -0300

    add: pause em irrigation

--- src/services/evapotranspirationServices.js
+++ src/services/evapotranspirationServices.js
@@ -120,14 +120,14 @@ const verifyEvapotranspiration = async (plantingBedId, reads) => {
     console.log("Previsão de evapotranspiração (mm): ", newEtcRecord);
 
     //ajuste pra considerar os tempo de dessincronização do esp32
-    if (allowedHours(Number(hour), Number(minute))) {
+    // if (allowedHours(Number(hour), Number(minute))) {
       return await verifyIrrigation(
         OWPayload,
         plantingBed,
         avgSensor,
         Number(hour),
       );
-    }
+    // }
     return 0;
   } catch (err) {
     console.log("Erro ao calcular ETc: ", err);
@@ -252,6 +252,7 @@ const verifyIrrigation = async (OWPayload, plantingBed, avgSensor, hours) => {
             stage: {
               connect: { id: plantingBed.stage.id },
             },
+            pause: true
           },
         });
         return 0;
@@ -293,6 +294,7 @@ const verifyIrrigation = async (OWPayload, plantingBed, avgSensor, hours) => {
           stage: {
             connect: { id: plantingBed.stage.id },
           },
+          pause: false
         },
       });
       console.log(
